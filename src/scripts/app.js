f168f9f21f356e3712ebf748b080c1ae615e30e9
M R <6248874+mrbrirup@users.noreply.github.com>
2020-01-13 02:03:14 +0000

    Dialog tests

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -18,13 +18,36 @@ assembly.onReady(
 
     assembly.loadManifest(Mrbr.UI.Utils.Utils.componentManifest("Mrbr.UI.Containers.Desktop", true, true).concat([new entry(entry.FileTypes.Style, "Mrbr.UI.Style.Default")])).then(
         result => {
-            application.desktop = new Mrbr.UI.Containers.Desktop({ navlocation: "right" });
+            application.desktop = new Mrbr.UI.Containers.Desktop({ navlocation: "bottom" });
             //application.desktop.navlocation = "top";
             document.body.appendChild(application.desktop);
             //appContainer = new Mrbr.UI.Containers.AppContainer();
             //appContainer.on("appcontainer-resize", function (evt) { console.log(evt.source.width, evt.source.height) })
             //let dr = new Mrbr.UI.Test.AppDrawer();            
             //desktop.appendChild(dr);
+            let dialog;
+            function showDialog() {
+                //_statusDialog.textContent = 'Dialog showed...';
+                //_showDialogButton.disabled = true;
+                if (!dialog) {
+                    //var id = 'dialog';
+                    // Instanciate the Dialog Box
+                    //dialog = new DialogBox(id, callbackDialog);
+                    //let dialog = document.getElementById("dialog")
+                    let dialog = document.createElement("mrbr-ui-dialogs-dialog")
+                    dialog.style = 'min-width:400px; min-height:280px;'
+                    dialog.classList.add("dialog")
+                    dialog.id = "dialog"
+                    dialog.container = application.desktop.desktop;
+                    document.body.appendChild(dialog);
+                    window.requestAnimationFrame(() => {
+                        dialog.showDialog();
+                    })
+                }
+            }
+            showDialog();
+
+
         })
         .catch(error => { console.log(error) })
 })
